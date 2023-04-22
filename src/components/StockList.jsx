a2bca6a1447cff82aebc8af2d42acf3b81791dbb
Sonjuan <simset1212@gmail.com>
2023-04-22 21:55:17 +0900

    initial state

---
+++ src/components/StockList.jsx
@@ -0,0 +1,9 @@
+import {useState, useEffect} from "react"
+
+export const StockList = () => {
+    const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
+
+    
+
+    return <div>StockList</div>
+}
