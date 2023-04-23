aa72fe53b995dfb5af02147a0bbad1288553c27b
Sonjuan <simset1212@gmail.com>
2023-04-24 02:50:33 +0900

    showing stockChart in stockDetail Page

--- src/components/stockChart.jsx
+++ src/components/stockChart.jsx
@@ -1,6 +1,41 @@
-export const StockChart = ({chartData}) => {
-    const {day, week, year} = chartData
-    
+import Chart from "react-apexcharts"
 
-    return <h3>StockChart</h3>
+
+export const StockChart = ({chartData, symbol}) => {
+    const {day, week, year} = chartData
+    const options = {
+        title : {
+            text: symbol,
+            align: "center",
+            style : {
+                fontSize: "24px"
+            }
+        },
+        chart : {
+            id: "stock data",
+            animation : {
+                speed: 1300
+            }
+        },
+        xaxis : {
+            type:"datetime",
+            labels : {
+                datetimeUTC : false
+            }
+        },
+        tooltip : {
+            x : {
+                format: "MMM dd HH:MM"
+            }
+        }
+    }
+    const series = [{
+        name : symbol,
+        data : day
+    }]
+
+
+    return <div className="mt-5 p-4 shadow-sm bg-white">
+        <Chart options={options} series={series} type="area" width="100%"/>
+    </div>
 }
