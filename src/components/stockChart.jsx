9c32596245d1319b0975746eac61892818e17bf4
Sonjuan <simset1212@gmail.com>
2023-04-24 15:49:19 +0900

    adding stockChart color feature & faced Array length Bug

--- src/components/stockChart.jsx
+++ src/components/stockChart.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react"
 import Chart from "react-apexcharts"
 
 
 export const StockChart = ({chartData, symbol}) => {
-    const {day, week, year} = chartData
+    const [dateFormat, setDateFormat] = useState("24h") 
+    const { day, week, year } = chartData
+    
+    const determineTimeFormat = () => {
+        switch(dateFormat) {
+            case "24h" :
+                return day
+            case "7d" :
+                return week
+            case "1y" :
+                return year
+            default :
+                return day
+        }
+    }
+
+    const color = (determineTimeFormat()[determineTimeFormat().length - 1].y - determineTimeFormat()[0].y) > 0 ? "#26C2381" : "#ed3419"
+    const series = [{
+        name : symbol,
+        data : determineTimeFormat()
+    }]
+    
     const options = {
+        colors: [color],
         title : {
             text: symbol,
             align: "center",
@@ -29,13 +52,25 @@ export const StockChart = ({chartData, symbol}) => {
             }
         }
     }
-    const series = [{
-        name : symbol,
-        data : day
-    }]
+    
+    
+    const renderButtonSelect = (button) => {
+        const classes = "btn m-1 "
+        if(button === dateFormat) {
+            return classes + "btn-primary"
+        }else {
+            return classes + "btn-outline-primary"
+        }
+    }
+
 
 
     return <div className="mt-5 p-4 shadow-sm bg-white">
         <Chart options={options} series={series} type="area" width="100%"/>
+        <div>
+            <button className={renderButtonSelect("24h")} onClick={() => setDateFormat("24h")}>24h</button>
+            <button className={renderButtonSelect("7d" )} onClick={() => setDateFormat("7d")}>7d</button>
+            <button className={renderButtonSelect("1y" )} onClick={() => setDateFormat("1y")}>1y</button>
+        </div>
     </div>
 }
